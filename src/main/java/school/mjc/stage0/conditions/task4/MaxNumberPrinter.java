60d49cb878711cc84ac893f8da9eda0edf3c2b93
AudriusAnusauskas <aaaudrius@gmail.com>
2023-05-27 16:27:49 +0300

    finish task 4 module 4.1 stage 0

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if ((first > second && first > third) || (first == second && first > third) || (first == third && first > second)){
+            System.out.println(first);
+        } else if ((second > first && second > third) || (second == first && second > third) || (second == third && second > second)){
+            System.out.println(second);
+        } else System.out.println(third);
     }
 }
